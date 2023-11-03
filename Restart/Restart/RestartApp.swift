d0d9ef3e9524146f0fbac0479f2a4e0d36c1ce5d
Javier Rodriguez Ardila <javierodriguezar@gmail.com>
2023-11-03 14:54:43 +0100

    Initial commit

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Javier Eduardo Rodriguez Ardila on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
